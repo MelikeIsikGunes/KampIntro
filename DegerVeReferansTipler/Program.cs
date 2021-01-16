e6f16c0c4a02c9db8267631e451ec0aacca446e0
Melike Işık <melike.34.37@hotmail.com>
2021-01-16 23:54:01 +0300

    16 Ocak Ders sonu

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int, decimal, float, double, bool = değer tip -> değer tipler bellekte stack alanında tutulur.
+            // array, class, interface = referans tip
+
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+            //sayi1 = 30 oldu
+
+            int[] sayilar1 = new int[] { 10, 20, 30 }; //dizinin adı stack alanında, içindeki değerler heap alanında tutulur.
+            int[] sayilar2 = new int[] { 100, 200, 300 };
+            sayilar1 = sayilar2;  //sayilar2'nin referans numarası sayilar1'e atandı. Yani artık ikisi de heap alanında aynı adrese gider.
+            sayilar2[0] = 999;
+            // sayilar1[0] = 999 oldu.
+
+        }
+    }
+}
---
+++ Metotlar/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Metotlar tekrar tekrar kullanılabilirliği sağlar.
+
+            Urun urun1 = new Urun();  //Class tanımlamak - Class'ın örneği oluşturmak - instance
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1,urun2 };
+
+            // C# type safe - tip güvenli bir dildir.
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("-----------------");
+            }
+
+            Console.WriteLine("--------Metotlar----------");
+            //encapsulation - Kapsülleme ->  Örnek : Ekle2 metotu yerine Ekle metotunu kullanmamız.
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut","Yeşil armut", 12, 10);
+            sepetManager.Ekle2("Elma", "Yeşil elma", 12, 9);
+            sepetManager.Ekle2("Karpuz", "Diyarbakır karpuzu", 12, 8);
+
+
+        }
+    }
+}
+
+
+// Don't repeat yourself - DRY - Clean Code - Best Practice
