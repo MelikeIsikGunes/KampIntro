19c292a62b37b0549eaf846e0263500408c9932b
Melike Işık <melike.34.37@hotmail.com>
2021-01-22 21:44:58 +0300

    20 Ocak Ders Sonu

--- GenericsIntro/MyList.cs
+++ GenericsIntro/MyList.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace GenericsIntro
 {
-    class MyList<T> //List sınıfını kendimiz oluşturuyoruz. (List kullanımı Koleksiyonlar projesinde)
+    class MyList<T> //Generic Class.List sınıfını kendimiz oluşturuyoruz. (List kullanımı Koleksiyonlar projesinde)
     {
         T[] items;
         public MyList()  //constructer metodu. Class ismi ile aynıdır. İlk çalışan metot. Program.cs'de "new MyList();" dendiği anda burası çalışır.
@@ -14,11 +14,11 @@ namespace GenericsIntro
         public void Add(T item) //Programcı hangi tip verirse 
         {
             T[] tempArray = items;  //geçici dizi oluşturduk. items referansını(adresini) tutması için.
-            items = new T[items.Length+1]; //Eleman sayısını 1 arttır. 
+            items = new T[items.Length+1]; //Eleman sayısını 1 arttır. (Burda yeni adres nolu boş bir dizi oluşur)
 
             for (int i = 0; i < tempArray.Length; i++)
             {
-                items[i] = tempArray[i]; //geçici olarak tuttuğumuz önceki elemanları items dizisine geri yüklüyoruz.
+                items[i] = tempArray[i]; //geçici olarak tuttuğumuz önceki elemanları items dizisine aktarıyoruz.
             }
 
             items[items.Length - 1] = item; // Add ile gelen elemanı ekle -> items'deki son index numarasına. (Örn: items.Length 6 ise son elemanın index nosu 5'tir. O yüzden Length-1 yazıyoruz.)
